278ad3c1071cbeae25019c1fc1aec47e38af407a
Kavindu Karunaratne <kavindu.kaumal@gmail.com>
2024-01-18 18:30:41 +0530

    player creation

--- main.js
+++ main.js
@@ -10,13 +10,31 @@ canvas.height = 576;
 c.fillStyle = 'white';
 c.fillRect(0, 0, canvas.width, canvas.height);
 
-//Import and render map
+// Import and render map
 const image = new Image();
 image.src = './images/Pellet Town.png';
 
+// Import and render Player
+const playerImage = new Image();
+playerImage.src = './images/playerDown.png'
+
 image.onload = () => {
-    c.drawImage(image, -750, -550);
+    c.drawImage(image, -735, -600);
+    c.drawImage(
+        // Player Crop
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        // Player actual
+        canvas.width / 2 - (playerImage.width / 4 / 2), 
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4,
+        playerImage.height
+    );
 }
+
 
 
 // console.log(image);

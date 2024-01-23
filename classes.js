45e62af7da529fdb70fde61e07c06f27b54f8136
Kavindu Karunaratne <kavindu.kaumal@gmail.com>
2024-01-23 09:31:56 +0530

    tackle attack for player and enemy

--- classes.js
+++ classes.js
@@ -8,7 +8,8 @@ class Sprite {
             hold: 10 
         }, 
         sprites = [],
-        animate = false 
+        animate = false,
+        isEnemy = false
     }) {
         this.position = position;
         this.image = image;
@@ -19,9 +20,14 @@ class Sprite {
         }
         this.animate = animate
         this.sprites = sprites
+        this.opacity = 1
+        this.health = 100
+        this.isEnemy = isEnemy
     }
 
     draw() {
+        c.save();
+        c.globalAlpha = this.opacity
         c.drawImage(
             // Player Crop
             this.image,
@@ -35,6 +41,7 @@ class Sprite {
             this.image.width / this.frames.max,
             this.image.height
         );
+        c.restore();
         if (!this.animate) return
         if (this.frames.max > 1) {
             this.frames.elapsed++
@@ -43,6 +50,45 @@ class Sprite {
             if (this.frames.val < this.frames.max - 1) this.frames.val++;
             else this.frames.val = 0;
         }
+    }
+
+    attack({attack, recipient}) {
+        const tl = gsap.timeline();
+
+        this.health -= attack.damage
+
+        let movementDistance = 20;
+        if (this.isEnemy) movementDistance = -20
+
+        let healthBar = '#enemyHealthBar'
+        if (this.isEnemy) healthBar = '#playerHealthBar'
+
+        tl.to(this.position, {
+            x: this.position.x - movementDistance
+        }).to(this.position, {
+            x: this.position.x + movementDistance * 2,
+            duration: 0.1,
+            onComplete: () => {
+                // Enemy gets hit
+                gsap.to(healthBar, {
+                    width: this.health - attack.damage + '%'
+                })
+                gsap.to(recipient.position, {
+                    x: recipient.position.x + 10,
+                    yoyo: true,
+                    repeat: 5,
+                    duration: 0.08,
+                })
+                gsap.to(recipient, {
+                    opacity: 0,
+                    repeat: 5,
+                    yoyo: true,
+                    duration: 0.08
+                }) 
+            }
+        }).to(this.position, {
+            x: this.position.x
+        })
     }
 }
 
--- main.js
+++ main.js
@@ -346,7 +346,8 @@ const draggle = new Sprite({
         max: 4,
         hold: 30
     },
-    animate: true
+    animate: true,
+    isEnemy: true
 });
 
 const embyImage = new Image();
@@ -372,6 +373,19 @@ function animateBattle() {
 }
 
 animateBattle();
+
+document.querySelectorAll('button').forEach((button) => {
+    button.addEventListener('click', () => {
+        emby.attack({
+            attack: {
+                name: 'Tackle',
+                damage: 10,
+                type: 'Normal'
+            },
+            recipient: draggle
+        });
+    })
+});
 
 let lastKey = '';
 
